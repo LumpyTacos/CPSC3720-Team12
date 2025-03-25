e80f321f0c70a89ae0caae07c67280deee1bed63
Kevin Lam <83970656+LumpyTacos@users.noreply.github.com>
2025-03-25 16:41:26 -0400

    Update accountFunction.py

--- accountFunction.py
+++ accountFunction.py
@@ -22,7 +22,14 @@ REQUEST_METHOD = {
 
 STATUS_CODE = {
     "SUCCESS": 200,
+    "CREATED": 201,
+    "ACCEPTED": 202,
+    "NO_CONTENT": 204,
+    "BAD_REQUEST": 400,
+    "UNAUTHORIZED": 401,
+    "FORBIDDEN": 403,
     "NOT_FOUND": 404,
+    "REQUEST_TIMEOUT": 408,
 }
 
 # Paths
